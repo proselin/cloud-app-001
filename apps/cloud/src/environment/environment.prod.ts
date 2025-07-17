d8e739f5a0ba761119500dcefe6811a07e7bd3ae
Nguyễn Quốc Hưng <72499558+proselin@users.noreply.github.com>
2025-07-17 22:37:25 +0700

    feat: add production config

--- apps/cloud/src/environment/environment.prod.ts
+++ apps/cloud/src/environment/environment.prod.ts
@@ -1,7 +1,7 @@
 import { EnvironmentModel } from '../app/common/config/environment.token';
 
 export const environment: EnvironmentModel = {
-  apiUrl: 'http://localhost:19202/api/v1',
-  staticImgsUrl: 'http://localhost:19202/static/imgs',
+  apiUrl: 'https://api-margiet.proselin.id.vn/api/v1',
+  staticImgsUrl: 'https://api-margiet.proselin.id.vn/static/imgs',
   production: false,
 };
